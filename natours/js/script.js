87f8d1488b772988bf6be61a5a0efa6d9e25d488
Erlan Carreira <erlancarreira@hotmail.com>
2018-08-07 01:24:57 -0300

    Projeto do Curso Advanced Css

---
+++ natours/js/script.js
@@ -0,0 +1,10 @@
+ O volume na unidade C n∆o tem nome.
+ O N£mero de SÇrie do Volume Ç 5818-38B9
+
+ Pasta de C:\wamp64\www\advanced-css\js
+
+11/06/2018  19:53    <DIR>          .
+11/06/2018  19:53    <DIR>          ..
+11/06/2018  19:53                 0 script.js
+               1 arquivo(s)              0 bytes
+               2 pasta(s)   1.871.005.446.144 bytes dispon°veis
